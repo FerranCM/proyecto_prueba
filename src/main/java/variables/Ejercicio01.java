6b23b754bb4c86a9d0bd3ca9b33252b23baef1eb
FerranCM <castello.ferran@gmail.com>
2023-01-16 18:12:45 +0100

    a todos

---
+++ src/main/java/variables/Ejercicio01.java
@@ -0,0 +1,21 @@
+package variables;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+    /*
+    Programa para definir variables y sacar su valor por pantalla
+     */
+    int numero = 1;
+    short marianico = 31210;
+    long gasol = 1000*marianico;
+    boolean indepencia = true;
+    double presupuesto = -1.5*10;
+    float longitud = -3*100;
+        System.out.println(numero);
+        System.out.println(marianico);
+        System.out.println(gasol);
+        System.out.println(indepencia);
+        System.out.println(presupuesto);
+        System.out.println(longitud);
+    }
+}
